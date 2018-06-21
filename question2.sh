a6508b06c4a340744c3a9b1e5d03c714e6cd55f2
khaleeqArshad1 <36642608+khaleeqArshad1@users.noreply.github.com>
2018-06-21 21:14:14 +0500

    Add files via upload

--- question2.sh
+++ question2.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+count=1
+
+  for i in `more -1 $1`
+	do
+
+	x=$(($count%2))
+
+		  if [ $x -eq 0 ]
+			then
+		      
+		        `touch evenfile.txt`
+			echo $i 1>>evenfile.txt
+			count=`expr $count + 1`
+
+			else
+		      
+		         `touch oddfile.txt`
+			echo $i 1>>oddfile.txt
+			count=`expr $count + 1`
+
+		  fi
+   done
+echo "evenfile Created!"
+echo "oddfile Created!"
